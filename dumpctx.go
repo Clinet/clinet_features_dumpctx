8f2302aa9e2dd94e4cfe2e096bc6ed9afcd21984
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2022-11-15 04:28:41 -0500

    dumpctx: Initial commit

---
+++ dumpctx.go
@@ -0,0 +1,20 @@
+package dumpctx
+
+import (
+	"github.com/Clinet/clinet_cmds"
+	"github.com/Clinet/clinet_features"
+)
+
+var Feature = features.Feature{
+	Name: "dumpctx",
+	Cmds: []*cmds.Cmd{
+		cmds.NewCmd("dumpctx", "DEBUG: Dumps command context as built by Clinet", nil).AddSubCmds(
+			cmds.NewCmd("sub1", "Test subcommand 1", handleDumpCtx),
+			cmds.NewCmd("sub2", "Test subcommand 2", handleDumpCtx),
+		),
+	},
+}
+
+func handleDumpCtx(ctx *cmds.CmdCtx) *cmds.CmdResp {
+	return cmds.NewCmdRespEmbed("Dump of ctx (*cmds.CmdCtx)", "```JSON\n" + ctx.String() + "```")
+}
